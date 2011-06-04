15a9bd4fd8bf867d2090fd7162249d8e47e6d3fd
Mark Dalgleish <mark.john.dalgleish@gmail.com>
2011-06-04 20:33:25 +1000

    Drawing polygons, fresher ui

--- js/script.js
+++ js/script.js
@@ -19,7 +19,7 @@
 	//Google Maps code
 	$(function(){
 	
-		var initialLocation;
+		var location;
 		var siberia = new google.maps.LatLng(60, 105);
 		var newyork = new google.maps.LatLng(40.69847032728747, -73.9514422416687);
 		var browserSupportFlag =  new Boolean();
@@ -35,22 +35,15 @@
 		  };
 		  map = new google.maps.Map(document.getElementById("map"), myOptions);
 		  geocoder = new google.maps.Geocoder();
-		  var blueimg = new google.maps.MarkerImage("http://www.google.com/intl/en_us/mapfiles/ms/icons/red-dot.png");
 			marker = new google.maps.Marker({
-				map: map,
-				draggable: true,
-				icon: blueimg
-				
+				map: map
 			});
 		  
 		  // Try W3C Geolocation method (Preferred)
 		  if(navigator.geolocation) {
 			browserSupportFlag = true;
 			navigator.geolocation.getCurrentPosition(function(position) {
-			  initialLocation = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
-			  map.setCenter(initialLocation);
-			  infowindow.setPosition(initialLocation);
-			  infowindow.open(map);
+			  dropPin(position.coords.latitude,position.coords.longitude);
 			}, function() {
 			  handleNoGeolocation(browserSupportFlag);
 			});
@@ -59,10 +52,7 @@
 			browserSupportFlag = true;
 			var geo = google.gears.factory.create('beta.geolocation');
 			geo.getCurrentPosition(function(position) {
-			  initialLocation = new google.maps.LatLng(position.latitude,position.longitude);
-			  map.setCenter(initialLocation);
-			  infowindow.setPosition(initialLocation);
-			  infowindow.open(map);
+				dropPin(position.latitude,position.longitude);
 			}, function() {
 			  handleNoGeolocation(browserSupportFlag);
 			});
@@ -75,15 +65,15 @@
 		 
 		function handleNoGeolocation(errorFlag) {
 		  if (errorFlag == true) {
-			initialLocation = newyork;
+			location = newyork;
 			contentString = "Error: The Geolocation service failed.";
 		  } else {
-			initialLocation = siberia;
+			location = siberia;
 			contentString = "Error: Your browser doesn't support geolocation. Are you in Siberia?";
 		  }
-		  map.setCenter(initialLocation);
+		  map.setCenter(location);
 		  infowindow.setContent(contentString);
-		  infowindow.setPosition(initialLocation);
+		  infowindow.setPosition(location);
 		  infowindow.open(map);
 		}
 		
@@ -105,13 +95,73 @@
 		  },
 		  //This bit is executed upon selection of an address
 		  select: function(event, ui) {
-			$("#latitude").val(ui.item.latitude);
-			$("#longitude").val(ui.item.longitude);
-			var location = new google.maps.LatLng(ui.item.latitude, ui.item.longitude);
-			marker.setPosition(location);
-			map.setCenter(location);
+			dropPin(ui.item.latitude, ui.item.longitude);
 		  }
 		});
+		
+		var layer,
+			circle,
+			polygons = [];
+		
+		function dropPin(latitude, longitude) {
+			location = new google.maps.LatLng(latitude, longitude);
+			marker.setPosition(location);
+			map.setCenter(location);
+			map.setZoom(11);
+			
+			if (!circle) {
+				circle = new google.maps.Circle({
+					clickable: false,
+					radius: 10000,
+					map: map,
+					fillOpacity: 0.2,
+					strokeOpacity: 0.5,
+					strokeWeight: 1
+				  });
+			};
+			circle.setCenter(location);
+			
+			clearPolygons();
+			
+			drawPolygon("-37.817921 145.033951 -37.818463 145.05043 -37.842326 145.053864 -37.842868 145.039444 -37.817921 145.033951");
+			drawPolygon("-37.843953 145.020218 -37.851543 145.041504 -37.865097 145.025024 -37.843953 145.020218");
+			drawPolygon("-37.776142 145.057297 -37.784283 145.067596 -37.769629 145.086823 -37.761487 145.07309 -37.776142 145.057297");
+		}
+		
+		function clearPolygons() {
+			$.each(polygons, function() {
+				this.setMap(null);
+			});
+		}
+		
+		function drawPolygon(polystring){
+			var polygon1;
+			var arr = new Array();
+				arr = polystring.split(" ");
+			var i=0;
+			var j=0;
+			var polycoords = new Array();
+			while(i<arr.length){
+				polycoords[j] = new google.maps.LatLng(arr[i],arr[i+1]);
+				
+				i = i+2;
+				j++;
+			}
+			
+			polygon1 = new google.maps.Polygon({
+				paths: polycoords,
+				strokeColor: "#FF0000",
+				strokeOpacity: 0.8,
+				strokeWeight: 3,
+				fillColor: "#FF0000",
+				fillOpacity: 0.35,
+				contentString: "Testing"
+			});
+			
+			polygon1.setMap(map);
+
+			polygons.push(polygon1);
+		}
 		
 	});
 
